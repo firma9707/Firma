fb87821d131ecc1bd6727fe0e358aedceca27e16
firma9707 <165617093+firma9707@users.noreply.github.com>
2024-04-01 11:25:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('second-Banner');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('fadeImage1');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('fadeMainText1');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('third-Banner');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('fadeImage2');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('fadeMainText2');
+    var position = element.getBoundingClientRect();
+
+    // 화면 내에 요소가 보이는지 확인
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        element.style.opacity = 1; // 이미지를 보이게 변경
+    }
+});
